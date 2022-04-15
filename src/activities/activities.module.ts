8dd67c5557273ba139bf48ace4b079bc2eabd1a8
fcesc-code <bpfran@gmail.com>
2022-04-15 22:47:39 +0200

    config / add mongoose schemas for activity module as features in module imports array

--- src/activities/activities.module.ts
+++ src/activities/activities.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ActivitiesController } from './controllers/activities.controller';
 import { ActivitiesService } from './services/activities.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BestOptionActivitySchema } from './schemas/best-option.activity.schema';
+import { SelectTextActivitySchema } from './schemas/select-text.activity.schema';
+import { TransformAspectActivitySchema } from './schemas/transform-aspect.activity.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'BestOptionActivitySchema',
+        schema: BestOptionActivitySchema,
+      },
+    ]),
+    MongooseModule.forFeature([
+      {
+        name: 'SelectTextActivitySchema',
+        schema: SelectTextActivitySchema,
+      },
+    ]),
+    MongooseModule.forFeature([
+      {
+        name: 'TransformAspectActivitySchema',
+        schema: TransformAspectActivitySchema,
+      },
+    ]),
+  ],
   controllers: [ActivitiesController],
   providers: [ActivitiesService], // add here ActivitiesMapper
   exports: [ActivitiesService],
