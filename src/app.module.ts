b0dc9c11660842c83f230732ba635d7bc7d79aac
fcesc-code <bpfran@gmail.com>
2022-04-15 22:46:31 +0200

    feature / add database connections to nestjs main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  DB_ACTIVITIES_KEYS,
+  DB_ACTIVITIES_NAME,
+  DB_GROUPS_KEYS,
+  DB_GROUPS_NAME,
+  DB_USERS_KEYS,
+  DB_USERS_NAME,
+} from 'KEYS/BBDD.KEYS';
 import { ActivitiesModule } from './activities/activities.module';
 
 @Module({
-  imports: [ActivitiesModule],
+  imports: [
+    ActivitiesModule,
+    MongooseModule.forRoot(DB_ACTIVITIES_KEYS, {
+      connectionName: DB_ACTIVITIES_NAME,
+    }),
+    MongooseModule.forRoot(DB_GROUPS_KEYS, {
+      connectionName: DB_GROUPS_NAME,
+    }),
+    MongooseModule.forRoot(DB_USERS_KEYS, {
+      connectionName: DB_USERS_NAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
