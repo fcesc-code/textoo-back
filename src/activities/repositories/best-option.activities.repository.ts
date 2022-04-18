58ff29445003f4634f1b129487a349e75a5c170d
fcesc-code <bpfran@gmail.com>
2022-04-18 18:53:52 +0200

    refactor / improve types in best-option activity repository and select-text activity repository

--- src/activities/repositories/best-option.activities.repository.ts
+++ src/activities/repositories/best-option.activities.repository.ts
@@ -3,8 +3,11 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { SharedActivitiesService } from '../services/shared.activities.service';
 import { MODELS } from '../models/activities.models';
-import { BestOptionActivityDto } from '../dtos/best-option.activity.dto';
-import { ActivityDto, NewActivityDto } from '../dtos/activities.dto';
+import {
+  BestOptionActivityDto,
+  NewBestOptionActivityDto,
+} from '../dtos/best-option.activity.dto';
+import { ActivityDto } from '../dtos/activities.dto';
 
 @Injectable()
 export class BestOptionActivitiesRepository {
@@ -14,28 +17,31 @@ export class BestOptionActivitiesRepository {
     private activitiesService: SharedActivitiesService,
   ) {}
 
-  async create(user: NewActivityDto): Promise<ActivityDto> {
-    const userWithId: ActivityDto = this.activitiesService.addId(user);
-    const data = await this.BestOptionActivityModel.create(userWithId);
+  async create(
+    activity: NewBestOptionActivityDto,
+  ): Promise<BestOptionActivityDto> {
+    const activityWithId: ActivityDto = this.activitiesService.addId(activity);
+    const data: BestOptionActivityDto =
+      await this.BestOptionActivityModel.create(activityWithId);
     return data;
   }
 
-  async delete(id: string): Promise<ActivityDto> {
-    const data = await this.BestOptionActivityModel.findByIdAndDelete(
-      id,
-    ).exec();
+  async delete(id: string): Promise<BestOptionActivityDto> {
+    const data: BestOptionActivityDto =
+      await this.BestOptionActivityModel.findByIdAndDelete(id).exec();
     return data;
   }
 
   async update(
     activityId: string,
-    changes: Partial<ActivityDto>,
-  ): Promise<ActivityDto> {
-    const data = await this.BestOptionActivityModel.findByIdAndUpdate(
-      { _id: activityId },
-      changes,
-      { returnOriginal: false },
-    ).exec();
+    changes: Partial<BestOptionActivityDto>,
+  ): Promise<BestOptionActivityDto> {
+    const data: BestOptionActivityDto =
+      await this.BestOptionActivityModel.findByIdAndUpdate(
+        { _id: activityId },
+        changes,
+        { returnOriginal: false },
+      ).exec();
     return data;
   }
 }
--- src/activities/repositories/select-text.activities.repository.ts
+++ src/activities/repositories/select-text.activities.repository.ts
@@ -3,8 +3,11 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { SharedActivitiesService } from '../services/shared.activities.service';
 import { MODELS } from '../models/activities.models';
-import { SelectTextActivityDto } from '../dtos/select-text-activity.dto';
-import { ActivityDto, NewActivityDto } from '../dtos/activities.dto';
+import {
+  NewSelectTextActivityDto,
+  SelectTextActivityDto,
+} from '../dtos/select-text-activity.dto';
+import { ActivityDto } from '../dtos/activities.dto';
 
 @Injectable()
 export class SelectTextActivitiesRepository {
@@ -14,28 +17,31 @@ export class SelectTextActivitiesRepository {
     private activitiesService: SharedActivitiesService,
   ) {}
 
-  async create(user: NewActivityDto): Promise<ActivityDto> {
-    const userWithId: ActivityDto = this.activitiesService.addId(user);
-    const data = await this.SelectTextActivityModel.create(userWithId);
+  async create(
+    activity: NewSelectTextActivityDto,
+  ): Promise<SelectTextActivityDto> {
+    const activityWithId: ActivityDto = this.activitiesService.addId(activity);
+    const data: SelectTextActivityDto =
+      await this.SelectTextActivityModel.create(activityWithId);
     return data;
   }
 
-  async delete(id: string): Promise<ActivityDto> {
-    const data = await this.SelectTextActivityModel.findByIdAndDelete(
-      id,
-    ).exec();
+  async delete(id: string): Promise<SelectTextActivityDto> {
+    const data: SelectTextActivityDto =
+      await this.SelectTextActivityModel.findByIdAndDelete(id).exec();
     return data;
   }
 
   async update(
     activityId: string,
-    changes: Partial<ActivityDto>,
-  ): Promise<ActivityDto> {
-    const data = await this.SelectTextActivityModel.findByIdAndUpdate(
-      { _id: activityId },
-      changes,
-      { returnOriginal: false },
-    ).exec();
+    changes: Partial<SelectTextActivityDto>,
+  ): Promise<SelectTextActivityDto> {
+    const data: SelectTextActivityDto =
+      await this.SelectTextActivityModel.findByIdAndUpdate(
+        { _id: activityId },
+        changes,
+        { returnOriginal: false },
+      ).exec();
     return data;
   }
 }
