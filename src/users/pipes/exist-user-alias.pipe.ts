559b3dd7eab883601ab2ea4646134b0577cd72f4
fcesc-code <bpfran@gmail.com>
2022-04-18 22:15:18 +0200

    feature / improve type definition in existuseralias pipe from users module

--- src/users/pipes/exist-user-alias.pipe.ts
+++ src/users/pipes/exist-user-alias.pipe.ts
@@ -1,11 +1,11 @@
 import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
-import { UserConstructor } from '../dtos/users.dtos';
+import { UserDto } from '../dtos/users.dtos';
 import { UsersRepository } from '../repositories/users.repository';
 
 @Injectable()
 export class ExistUserAliasPipe implements PipeTransform {
   constructor(private usersDB: UsersRepository) {}
-  async transform(value: UserConstructor) {
+  async transform(value: Partial<UserDto>) {
     const exists: boolean = await this.usersDB.aliasAlreadyExist(value);
 
     if (exists) {
