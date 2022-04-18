6349143e85a277a2fe43a1452b2bb1ac40be24f9
fcesc-code <bpfran@gmail.com>
2022-04-18 18:49:35 +0200

    feature / add funcitonality to transform-aspect activity controller and transform-aspect activity repository

--- src/activities/controllers/transform-aspect.activities.controller.ts
+++ src/activities/controllers/transform-aspect.activities.controller.ts
@@ -1,24 +1,74 @@
 import {
   Controller,
-  Get,
-  // Body,
-  // Delete,
-  // Get,
-  // Param,
-  // Post,
-  // Put,
+  HttpStatus,
+  Body,
+  Delete,
+  Param,
+  Post,
+  Put,
+  Res,
 } from '@nestjs/common';
-import { ActivityDto } from '../dtos/activities.dto';
+import {
+  TransformAspectActivityDto,
+  NewTransformAspectActivityDto,
+} from '../dtos/transform-aspect.activity.dto';
 import { TransformAspectActivitiesRepository } from '../repositories/transform-aspect.activities.repository';
+import { SharedActivitiesRepository } from '../repositories/shared.activities.repository';
+import { MODELS } from '../models/activities.models';
 
-@Controller('activities')
+@Controller(`${MODELS.root}/${MODELS.selectText}`)
 export class TransformAspectActivitiesController {
   constructor(
-    private transformAspectActivitiesDB: TransformAspectActivitiesRepository,
+    private activityDB: TransformAspectActivitiesRepository,
+    private sharedActivitiesDB: SharedActivitiesRepository,
   ) {}
 
-  @Get('/api/activities/transform-aspect')
-  async getAllTransformAspectActivities(): Promise<ActivityDto[]> {
-    return this.transformAspectActivitiesDB.findAllTransformAspectActivity();
+  @Post()
+  async createTransformAspectActivity(
+    @Res() res,
+    @Body() activity: NewTransformAspectActivityDto,
+  ): Promise<TransformAspectActivityDto> {
+    const data = await this.activityDB.create(activity);
+    return res.status(HttpStatus.CREATED).json({
+      message: 'TransformAspectActivity Successfully Created',
+      activity: data,
+    });
+  }
+
+  @Delete('/:activityId')
+  async deleteTransformAspectActivity(
+    @Res() res,
+    @Param('activityId') activityId: string,
+  ): Promise<TransformAspectActivityDto> {
+    const idExists = await this.sharedActivitiesDB.findById(activityId);
+    if (!idExists) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: 'TransformAspectActivity does not exist',
+      });
+    }
+    const data = await this.activityDB.delete(activityId);
+    return res.status(HttpStatus.OK).json({
+      message: 'TransformAspectActivity Successfully Deleted',
+      activity: data,
+    });
+  }
+
+  @Put('/:activityId')
+  async updateTransformAspectActivity(
+    @Res() res,
+    @Param('activityId') activityId: string,
+    @Body() changes: Partial<TransformAspectActivityDto>,
+  ): Promise<TransformAspectActivityDto> {
+    const activityExists = await this.sharedActivitiesDB.findById(activityId);
+    if (!activityExists) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: 'TransformAspectActivity does not exist',
+      });
+    }
+    const data = await this.activityDB.update(activityId, changes);
+    return res.status(HttpStatus.OK).json({
+      message: 'TransformAspectActivity Successfully Updated',
+      activity: data,
+    });
   }
 }
