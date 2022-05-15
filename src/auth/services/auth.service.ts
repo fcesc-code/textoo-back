3950f0ccc7ee55bf0604b3621709a733d4223eb1
fcesc-code <bpfran@gmail.com>
2022-05-15 11:58:26 +0200

    hotfix / update links to user repository as relative

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
-import { UsersRepository } from 'src/users/repositories/users.repository';
+import { UsersRepository } from '../../users/repositories/users.repository';
 import { JWTPayload } from '../interfaces/auth.interfaces';
 
 @Injectable()
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,10 +1,10 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { UserDto } from 'src/users/dtos/users.dtos';
-import { UsersRepository } from 'src/users/repositories/users.repository';
+import { UserDto } from '../../users/dtos/users.dtos';
+import { UsersRepository } from '../../users/repositories/users.repository';
 import { JWTPayload } from '../interfaces/auth.interfaces';
-import SECRET_KEY from 'keys/SECRET.KEY';
+import SECRET_KEY from './../../keys/SECRET.KEY';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
